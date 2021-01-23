6e064f69ec5d03f2a5036d70279716243fff6ab3
mchausse <maxime.chausse.1@ens.etsmtl.ca>
2021-01-23 18:19:33 -0500

    test for the connection with the firebase DB

--- public/vues/main.php
+++ public/vues/main.php
@@ -17,7 +17,18 @@
 		if (ISSET($_SESSION["connected"])){
 			$compte=$cDao->find($_SESSION["connected"]);
 			$background=$compte->getBackground();
-		}
+        }
+        require __DIR__.'/vendor/autoload.php';
+        use Kreait\Firebase\Factory;
+        use Kreait\Firebase\ServiceAccount;
+        // This assumes that you have placed the Firebase credentials in the same directory
+        // as this PHP file.
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__ . '/secret/php-firebase-7f39e-c654ccd32aba.json');
+        $firebase = (new Factory)
+        ->withServiceAccount($serviceAccount)
+        ->withDatabaseUri('https://my-project.firebaseio.com')
+        ->create();
+        $database = $firebase->getDatabase();
 	?>
 	<body id="<?=$background?>">
 	<div id="menu">
